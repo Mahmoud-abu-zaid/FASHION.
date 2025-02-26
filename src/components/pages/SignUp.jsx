f641ba74c1ac76b095de39d59966c60353f95db8
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-02-26 21:07:13 +0200

    Add:sign up

--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -17,7 +17,7 @@ export default function SignUp() {
           <p className="py-4">Type your email down below and be young wild generation</p>
           <form onSubmit={(e)=> e.preventDefault()}>
           <label htmlFor="userEmail" className="bg-white p-4 rounded-lg">
-            <input className=" p-3 rounded-lg text-black" type="email" name="" id="userEmail" onChange={(e)=>setUserEmail(e.target.value)} />
+            <input className=" p-3 rounded-lg text-black" type="email" name="" id="userEmail" placeholder="Add your email here" required onChange={(e)=>setUserEmail(e.target.value)} />
             <button onClick={login} className="bg-black ml-2 p-2 px-4 rounded-lg">Send</button>
           </label>
           </form>
